e0efcc758314a5a5aab6935b8c0e16609a4368b5
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-16 11:48:38 +0530

    Added to CreateEmployeePayrollTable

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -4,3 +4,15 @@
 create database employee_payroll;
 
 use employee_payroll;
+
+-- UC2 Create employee_payroll table
+create table employee_payroll(
+-- id is incremented by one starting from 1, primary key(unique and not null)
+id int identity(1,1) primary key,
+-- name is not null
+name varchar(200) not null,
+-- salary is not null
+salary int not null,
+-- startDate stores date which is not null
+startDate date not null
+);
